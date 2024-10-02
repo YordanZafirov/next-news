25d2b67cbf27629eb884137c1d213d33c2aa3a5c
YordanZafirov <iordanzafirov17@gmail.com>
2024-10-02 21:31:15 +0300

    feat: adding the styles to the components

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 const NewsPage = () => {
   return (
     <div>
       <h1>News Page</h1>
+      <ul className="news-list">
+        <li>
+          <Link href="/news/1">News 1</Link>
+        </li>
+        <li>
+          <Link href="/news/2">News 2</Link>
+        </li>
+        <li>
+          <Link href="/news/3">News 3</Link>
+        </li>
+      </ul>
     </div>
   );
 };
