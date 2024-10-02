532de68d3da8b6c5e9fe31d4c89f7a408ddc40eb
YordanZafirov <iordanzafirov17@gmail.com>
2024-10-02 22:04:02 +0300

    feat: Update news details page to handle not found news items
    - Added import statement for the next/navigation module
    - Updated the NewsDetailsPage component to use the notFound function from next/navigation when a news item is not found
    - Created a new NotFoundPage component to display a 404 error message

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -1,13 +1,14 @@
 import { DUMMY_NEWS } from "@/dummy-news";
 import { INewsDetailsPage } from "@/static/types";
 import Image from "next/image";
+import { notFound } from "next/navigation";
 
 const NewsDetailsPage = ({ params }: INewsDetailsPage) => {
   const newsId = params.id;
   const newsItem = DUMMY_NEWS.find((news) => news.slug === newsId);
 
   if (!newsItem) {
-    return <p>News not found</p>;
+    notFound();
   }
 
   return (
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+const NotFoundPage = () => {
+    return ( 
+        <div id="error">
+            <h1>404 - Page Not Found</h1>
+            <p>The requested page could not be found.</p>
+        </div>
+     );
+}
+ 
+export default NotFoundPage;
